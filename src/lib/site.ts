a6214a1b565f7d2d7bfd25f5d5c4e900f28c648a
Ian Almeida <ianvazalmeida@gmail.com>
2026-09-22 11:55:46 -0400

    ajuste de copy para página portfólio

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,5 +1,5 @@
 export const WHATSAPP_URL_GENERIC = "https://wa.link/ndpnkx";
-export const WHATSAPP_URL_PORTFOLIO = "https://wa.link/ndpnkx";
+export const WHATSAPP_URL_PORTFOLIO = "https://wa.link/nw2iez";
 export const WHATSAPP_URL_LANDING = "https://wa.link/cpe6z7";
 export const WHATSAPP_URL_SITE_INSTITUCIONAL = "https://wa.link/65ofoj";
 export const WHATSAPP_URL_ORCAMENTO = "https://wa.link/2wivau";
